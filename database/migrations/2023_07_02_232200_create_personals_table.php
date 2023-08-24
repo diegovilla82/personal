23398d7212268a3fe82dd16c6becd5e2066c1335
VILLARROEL Diego (diego.villarroel) <diego.villarroel@chaco.gob.ar>
2023-08-24 11:10:57 -0300

    Se agregaron los campos nuevos a la bd

--- database/migrations/2023_07_02_232200_create_personals_table.php
+++ database/migrations/2023_07_02_232200_create_personals_table.php
@@ -16,8 +16,31 @@ return new class extends Migration
             $table->year('year_of_income')->nullable();
             $table->string('username')->nullable();
             $table->string('name');
-            $table->string('last_name');
+            $table->string('last_name')->nullable();
+            $table->string('full_name')->nullable();
+            $table->string('qualification_id')->nullable();
+            $table->unsignedBigInteger('position_id')->nullable();
+            $table->unsignedBigInteger('dependence_id')->nullable();
+            $table->unsignedBigInteger('city_id')->nullable();
+            $table->string('neighborhood')->nullable();
             $table->string('dni')->nullable();
+            //--licencia
+            //-- Antiguedad
+            $table->year('anti_year')->nullable();
+            $table->integer('anti_month')->nullable();
+            $table->integer('anti_day')->nullable();
+            $table->date('system_date')->nullable();
+            //-- Por ley 6039
+            $table->year('ley_6039_year')->nullable();
+            $table->integer('ley_6039_month')->nullable();
+            $table->integer('ley_6039_day')->nullable();
+            //-- Anses
+            $table->year('anses_year')->nullable();
+            $table->integer('anses_month')->nullable();
+            $table->integer('anses_day')->nullable();
+            //-- días de vacaciones
+            $table->integer('vacation_days')->nullable();
+
             $table->timestamps();
         });
     }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
@@ -25,5 +26,8 @@ class RoleSeeder extends Seeder
             'name' => 'user',
             'guard_name' => 'web'
         ]);
+
+        $user = User::find(1);
+        $user->assignRole('admin');
     }
 }
